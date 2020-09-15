5fbe209d48e7b987efda39a3d305b08ca5f206af
Ashley Berry <ashleywberry@gmail.com>
2020-09-15 17:47:36 -0500

    added compatability between app.js and other components

---
+++ src/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    render(){
+        return (
+            <h4>© React Actors</h4>
+        )
+    }
+} // end component
+
+export default Footer
---
+++ src/Header/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render(){
+        return (
+            <h1>React Actors</h1>
+        )
+    }
+} // end component
+
+export default Header
